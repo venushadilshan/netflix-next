562a69e9695887d81b3a58a456f3d09cb1ff6a3f
Venusha Dilshan Dushmantha <venusha125@gmail.com>
2021-12-20 18:30:20 +0530

    Change: Layout for mobile

--- components/LandingCover.js
+++ components/LandingCover.js
@@ -7,9 +7,9 @@ const LandingCover = () => {
 
         <div className="flex flex-row h-full items-center justify-between">
             <div className="flex flex-col items-start justify-center lg:ml-20">
-        <img src="https://pngpress.com/wp-content/uploads/2020/09/uploads_netflix_netflix_PNG11.png" className="w-32 lg:ml-1 p-0 z-20 ml-5" />
+        <img src="https://pngpress.com/wp-content/uploads/2020/09/uploads_netflix_netflix_PNG11.png" className="w-32 lg:ml-0 p-0 z-20 ml-5" />
         <h1 className="text-slate-50 ml-5 lg:text-6xl lg:ml-0 text-3xl font-bold z-20">INTERSTELLAR</h1>
-        <p className="text-slate-50 w-96 lg:ml-0 pl-5 text-md font-semibold ">When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.</p>        
+        <p className="text-slate-50 w-72 lg:ml-0 pl-5 text-md font-semibold ">When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.</p>        
         <div className="flex flex-row justify-start items-start mt-2">
         <button className="bg-gray-400/50 px-8 py-1 mt-2 rounded text-slate-50 font-semibold flex flex-row items-center text-md mx-2 hover:bg-gray-600"><BsFillPlayFill/> &nbsp;Play</button>
         <button className="bg-gray-400/50 px-8 py-1 mt-2 rounded text-slate-50 font-semibold flex flex-row items-center text-md mx-2 hover:bg-gray-600"><BsPlusLg/> &nbsp;My List</button>
--- pages/MovieDetails.js
+++ pages/MovieDetails.js
@@ -13,24 +13,24 @@ const MovieDetails = () => {
                <MovieBG image={router.query.movieCover}/>
             <NavBar />
          
-            <div className="flex flex-row items-center h-screen  z-20">
+            <div className="flex flex-row items-center h-screen  z-20 flex-wrap lg:flex-nowrap">
 
-                <div className="shadow-2xl z-20 flex relative bg-center bg-cover flex-col justify-center h-3/4 w-1/2 items-start ml-20 rounded" style={{ backgroundImage: "url('"+router.query.movieCover+"')" }}>
+                <div className="shadow-2xl z-20 flex relative bg-center bg-cover flex-col justify-center w-full h-72 lg:h-3/4 lg:w-1/2 items-start lg:ml-20 rounded" style={{ backgroundImage: "url('"+router.query.movieCover+"')" }}>
                     <div className="absolute bottom-0 mb-5  ml-5 flex flex-row z-20">
                     <h1 className=" text-slate-50 text-3xl font-bold">{router.query.movieName}</h1>
                     </div>
                     
                 </div>
-            <div className="flex flex-col items-start justify-start mt-20 h-3/4 w-full z-20">
+            <div className="flex flex-col items-start justify-start lg:mt-20 mt-2 lg:h-3/4 h-full w-full z-20">
             
             <h1 className=" text-slate-50 text-3xl ml-5 font-bold z-20">{router.query.movieName}</h1>
             <p className="text-slate-50 my-2 pl-5  pr-5 z-20"><span className="font-semibold">Votes: </span>{router.query.movieVotes} </p>
             <p className="text-slate-50 my-2 pl-5  pr-5 z-20"><span className="font-semibold">Release Date:</span> {router.query.movieDate} </p>
-            <div className="flex flex-row mt-5">
+            <div className="flex flex-row lg:mt-5">
             <button className="bg-red-700 ml-5 px-5 py-1 rounded text-slate-50 opacity-100 hover:opacity-100">Preview</button>
             <button className="bg-red-700 ml-5 px-5 py-1 rounded text-slate-50 opacity-100 hover:opacity-100">Add to My List</button>
             </div>
-              <p className="text-slate-50 mt-5  pl-5  pr-5 w-3/4 z-20 ">{router.query.movieDescription}</p>
+              <p className="text-slate-50 lgmt-5  pl-5  pr-5 w-3/4 z-20 ">{router.query.movieDescription}</p>
             </div>
 
             </div>
